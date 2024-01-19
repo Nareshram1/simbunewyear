33a06f2cfc36879aa3e3edc04685bc16ffa9d6d4
Nareshram1 <nireshram805s1@gmail.com>
2024-01-20 02:25:26 +0530

    Added topbar stuff favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 import Timer from "./timer";
-
+import Head from "next/head";
 export default function Home() {
   const targetDate = new Date('2024-02-01T00:00:00').getTime();
   const serverTime=new Date();
   // console.log(typeof(serverTime))
   // console.log(serverTime)
   return (
+    <div>
+   
+   <Head>
+  <title>STRrr</title>
+  <link rel="icon" href="/simbulogo.ico" /> 
+  </Head>
     <main className="flex justify-center items-center h-screen">
       
      <Timer targetDate={targetDate} serverTime={serverTime}/>
+     {/* <img src="/simbu.png"></img> */}
     </main>
+    </div>
   );
 }
